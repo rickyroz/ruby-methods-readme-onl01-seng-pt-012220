e9707d5752676ce2b493ce4fb02a228793475be0
Ricky Rozier <rickydrozier@gmail.com>
2020-01-27 01:39:17 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,8 +1,14 @@
-def greeting
-  puts "Hello World"
+def say_hello_world_ten_times
+  
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
-greeting
-greeting
-greeting
-greeting
-greeting
